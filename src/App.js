0baea57717a86f3c7c1e98ce20672842413c7213
maxcroy1 <max.croy1@gmail.com>
2020-08-12 09:41:47 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AMSynth } from 'tone';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state={
+    keymappings: {
+      a: "C4",
+      w: "C#4",
+      s: "D4",
+      e: "D#4",
+      d: "E4",
+      f: "F4",
+      t: "F#4",
+      g: "G4",
+      y: "G#4",
+      h: "A4",
+      u: "A#4", 
+      j: "B4",
+      k: "C5",
+      o: "C#5",
+      l: "D5",
+      p: "D#5",
+      ":": "E5",
+      "'": "F5"
+    },
+  }
+
+  componentDidMount() {
+    const synth = new AMSynth().toDestination();
+    const self = this;
+    function playKey(event) {
+      let key = event.key
+      if (self.state.keymappings[key]) {
+        synth.triggerAttackRelease(`${self.state.keymappings[key]}${self.state.octave}`)
+      }
+    }
+
+    document.addEventListener('keydown', playKey)
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
